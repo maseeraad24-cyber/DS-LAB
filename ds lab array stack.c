e8d2212285886e2032b700581fd5824bc37c02e4
maseeraad24-cyber <maseera.ad24@bmsce.ac.in>
2025-11-11 14:39:11 +0530

    Add files via upload

---
+++ ds lab array stack.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#define SIZE 10
+void push(int);
+void pop();
+void display();
+int stack[SIZE], top=-1;
+void main()
+{
+int value, choice;
+while(1){
+printf("\n\n*****MENU*****\n");
+printf("1.Push\n2.Pop\n3.Display\n4.Exit");
+printf("\nEnter your choice: ");
+scanf("%d",&choice);
+switch(choice){
+case 1:
+    printf("Enter value to insert: ");
+    scanf("%d",&value);
+    push(value);
+    break;
+case 2:
+    pop();
+    break;
+case3:
+    display();
+    break;
+case4:
+    exit(0);
+
+default:("\nWrong selection.");
+}
+}
+}
+void push(int value){
+if(top==SIZE-1)
+printf("\nStack is full");
+else{
+top++;
+stack[top]=value;
+printf("\nInsertion success");
+}
+}
+void pop(){
+if(top==-1)
+    printf("\nStack is empty. Deletion is not possible");
+else{
+    printf("\nDeleted:%d",stack[top]);
+    top--;
+}}
+void display(){
+if(top==-1)
+    printf("\nStack is empty.");
+else{
+    int i;
+    printf("\nStack elements are:\n");
+    for(i=top;i>=0;i--)
+    printf("%d\n",stack[i]);
+}
+}
